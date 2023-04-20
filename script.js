02314a8abc2cb583b429635674832f958b18f7ad
kevincallaghan <kevincallaghan5@yahoo.com>
2023-04-20 13:12:51 -0500

    nav animations and header text image

--- script.js
+++ script.js
@@ -160,3 +160,18 @@ function setFavicons(favImg) {
 }
 setFavicons('images/favicon.ico');
 
+// Source: Beyond Fireship Youtube Video "Subtle, yet Beautiful Scroll Animations" link: https://www.youtube.com/watch?v=T33NN_pPeNI
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add('showNav');
+    } else {
+      entry.target.classList.remove('showNav');
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.hiddenNav');
+hiddenElements.forEach((el) => observer.observe(el));
